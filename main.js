99302fee1d624440d8f46967f916baff1141b269
Samuel1242 <94230313+Samuel1242@users.noreply.github.com>
2022-08-19 10:51:22 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+img = "";
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+	img = loadImage("mario05.png");
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+
+	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+	video.size(800,400)
+	video.parent("game_console")
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded(){
+	console.log("Model Loaded!");
+}
+
+function gotPoses(results){
+	if(results > 0){
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+	}
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
